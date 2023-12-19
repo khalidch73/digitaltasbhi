161a6f722d0d61d82054c871fdcbcfb2fbaad132
khalidch73 <khalidnawazch73@gmail.com>
2023-12-19 08:38:26 +0500

    update

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -35,7 +35,7 @@ export const UpdateUI = () => {
       <Navbar/>
       <div className="relative flex justify-center">
         <div className="py-24">
-        <p className="font-bold text-slate-50 px-5 py-2 text-8xl">{value}</p>
+        <p className="font-bold text-slate-100 px-5 py-2 text-8xl">{value}</p>
         <span className="">
         <button className="bg-gradient-to-r from-red-900 to-yellow-300 p-10 rounded-full border transform transition-transform hover:scale-110" onClick={increment}>Count</button>
         </span>
