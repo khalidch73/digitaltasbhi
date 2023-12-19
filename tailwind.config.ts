2fba5c6b6c88ea9825f5a8a84300e2d642f4a9c7
khalidch73 <khalidnawazch73@gmail.com>
2023-12-19 09:54:57 +0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,22 @@ const config: Config = {
 
   ],
   theme: {
-    extend: {
+    screens: {
+      'sm': '340px',
+      // => @media (min-width: 340px) { ... }
 
-    },
+      'md': '748px',
+      // => @media (min-width: 748px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
